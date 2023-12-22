76272920668a04994c083a8a9281a38d0c0a6391
Anthony COUCKE <anthony.coucke@intm.fr>
2023-12-22 18:14:33 +0100

    chore: setup firebase

---
+++ public/env.js
@@ -0,0 +1,9 @@
+window.env = {
+  "FIREBASE_API_KEY": "AIzaSyCfFgA3FeCdA6QnlhjABI38GN7G6Cmrq2k",
+  "FIREBASE_AUTH_DOMAIN": "shoplist-dev-f95ed.firebaseapp.com",
+  "FIREBASE_DATABASE_URL": "https://shoplist-c6c1d-default-rtdb.europe-west1.firebasedatabase.app",
+  "FIREBASE_PROJECT_ID": "shoplist-dev-f95ed",
+  "FIREBASE_STORAGE_BUCKET": "shoplist-dev-f95ed.appspot.com",
+  "FIREBASE_MESSAGING_SERVER_ID": "553449455659",
+  "FIREBASE_APP_ID": "1:553449455659:web:4baa79c40ff9c02ef8ea6e"
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref } from 'firebase/database' 
+import firebaseConfig from './firebaseConfig';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+initializeApp(firebaseConfig);
+const database = getDatabase();
+
+const dbRef = ref(database);
 root.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
 
